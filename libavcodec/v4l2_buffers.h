4643111c56a93bf7c5f45fbd2a9c00954a6ad96d
Mark Kendall <mark.kendall@gmail.com>
2021-04-01 12:06:03 -0400

    MythTV: v4l2 changes
    Signed-off-by: Peter Bennett <pbennett@mythtv.org>
    (cherry picked from commit f1f21c96e808d3b0e0c7c0468927aeb46395f3f5)
    
    libavcodec/v4l2_m2m_dec.c: silence deprecation warning
    
    AVCodec::pix_fmts has been deprecated.  Use the new replacement function
    and add protection against out of bounds memory access.  I'm returning -1
    since I don't know what error code I should use.

--- libavcodec/v4l2_buffers.h
+++ libavcodec/v4l2_buffers.h
@@ -28,6 +28,7 @@
 #include <stddef.h>
 #include <linux/videodev2.h>
 
+#include "libavutil/hwcontext_drm.h"
 #include "libavutil/frame.h"
 #include "packet.h"
 
@@ -43,6 +44,9 @@ enum V4L2Buffer_status {
 typedef struct V4L2Buffer {
     /* each buffer needs to have a reference to its context */
     struct V4L2Context *context;
+
+    /* DRM descriptor */
+    AVDRMFrameDescriptor drm_frame;
 
     /* This object is refcounted per-plane, so we need to keep track
      * of how many context-refs we are holding.

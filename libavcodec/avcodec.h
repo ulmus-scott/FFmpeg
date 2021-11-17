72ce9c3aa45988975c2737b44af8a6fc9d8e25b2
ulmus-scott <scott.the.elm@gmail.com>
2021-11-17 15:10:40 -0500

    MythTV: MPEG multilingual audio support

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -1368,6 +1368,15 @@ typedef struct AVCodecContext {
     int refcounted_frames;
 #endif
 
+    /**
+     * set when bilingual audio data has been detected.
+     * 0 normally, 1 if dual language flag is set
+     *
+     * - encoding: unused (called delay in this case...)
+     * - decoding: set by lavc
+     */
+    int avcodec_dual_language;
+
     /* - encoding parameters */
     float qcompress;  ///< amount of qscale change between easy & hard scenes (0.0-1.0)
     float qblur;      ///< amount of qscale smoothing over time (0.0-1.0)

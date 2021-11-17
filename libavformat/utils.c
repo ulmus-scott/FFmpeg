8543dda853653dcfb7c627e34e3126d02be69b01
ulmus-scott <scott.the.elm@gmail.com>
2021-11-17 16:39:48 -0500

    libavformat/mpegts-mythtv

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -54,6 +54,15 @@ const char av_format_ffversion[] = "FFmpeg version " FFMPEG_VERSION;
 
 static AVMutex avformat_mutex = AV_MUTEX_INITIALIZER;
 
+/* MYTHTV CHANGES */
+extern AVInputFormat ff_mythtv_mpegts_demuxer;
+extern AVInputFormat ff_mythtv_mpegtsraw_demuxer;
+extern AVInputFormat ff_mpegts_demuxer;
+extern AVInputFormat ff_mpegtsraw_demuxer;
+
+/* END MYTHTV CHANGES */
+
+
 /**
  * @file
  * various utility functions for use within FFmpeg

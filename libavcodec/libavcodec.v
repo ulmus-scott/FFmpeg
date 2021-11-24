79ea8b0bb5970b2503746b74f8dd33a05a581a6a
ulmus-scott <scott.the.elm@gmail.com>
2021-11-23 21:41:42 -0500

    lavc: expose extra functions used in MythTV

--- libavcodec/libavcodec.v
+++ libavcodec/libavcodec.v
@@ -1,6 +1,12 @@
 LIBAVCODEC_MAJOR {
     global:
         av*;
+        ff_ue_golomb_vlc_code;
+        ff_golomb_vlc_len;
+        ff_se_golomb_vlc_code;
+        avpriv_find_start_code;
+        ff_fft_init;
+        ff_fft_end;
     local:
         *;
 };

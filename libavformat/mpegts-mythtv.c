d51c41ac83a9cc59881bd1961214575d70fd6159
Scott Theisen <scott.the.elm@gmail.com>
2022-02-14 20:00:40 -0500

    mpegts-mythtv.c: pmt_cb(): remove unused variable desc_count

--- libavformat/mpegts-mythtv.c
+++ libavformat/mpegts-mythtv.c
@@ -2605,7 +2605,6 @@ static void pmt_cb(MpegTSFilter *filter, const uint8_t *section, int section_len
     // MythTV
     int equal_streams = 0;
     int last_item = 0;
-    int desc_count = 0;
 
     pmt_entry_t items[PMT_PIDS_MAX];
     memset(&items, 0, sizeof(pmt_entry_t) * PMT_PIDS_MAX);
@@ -2758,8 +2757,6 @@ static void pmt_cb(MpegTSFilter *filter, const uint8_t *section, int section_len
                    sizeof(dvb_caption_info_t));
             last_item++;
         }
-
-        desc_count++;
     }
 
     // begin MythTV

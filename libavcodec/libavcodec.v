5acd2edcda701636fe03c0e59faee05ce6a660a8
Scott Theisen <scott.the.elm@gmail.com>
2022-06-13 15:53:10 -0400

    libavcodec/libavcodec.v: remove unnecessary function exports
    MythTV no longer uses these functions.

--- libavcodec/libavcodec.v
+++ libavcodec/libavcodec.v
@@ -1,11 +1,6 @@
 LIBAVCODEC_MAJOR {
     global:
         av*;
-        ff_ue_golomb_vlc_code;
-        ff_golomb_vlc_len;
-        ff_se_golomb_vlc_code;
-        ff_fft_init;
-        ff_fft_end;
     local:
         *;
 };

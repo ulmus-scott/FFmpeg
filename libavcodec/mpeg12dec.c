04de090305f9aeb88fa285c627a0cfc937dc60b0
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-10-23 14:29:47 +0200

    avcodec/mpeg12dec: Remove redundant function call
    Redundant since dcb29d37d4ffedc84e44df99f8d22ecf27e0f2cd.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/mpeg12dec.c
+++ libavcodec/mpeg12dec.c
@@ -2985,7 +2985,6 @@ static int mpeg_decode_frame(AVCodecContext *avctx, AVFrame *picture,
     }
 #endif
 
-    s2->codec_tag = ff_toupper4(avctx->codec_tag);
     if (s->mpeg_enc_ctx_allocated == 0 && (   s2->codec_tag == AV_RL32("VCR2")
                                            || s2->codec_tag == AV_RL32("BW10")
                                           ))

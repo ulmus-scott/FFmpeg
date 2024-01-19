3ced92557497991add67ea54a7688638d1fff99b
Anton Khirnov <anton@khirnov.net>
2024-01-19 13:33:28 +0100

    Revert "all: Don't set AVClass.item_name to its default value"
    Some callers assume that item_name is always set, so this may be
    considered an API break.
    
    This reverts commit 0c6203c97a99f69dbaa6e4011d48c331e1111f5e.

--- libavcodec/v4l2_m2m_dec.c
+++ libavcodec/v4l2_m2m_dec.c
@@ -280,6 +280,7 @@ static const AVCodecHWConfigInternal *v4l2_m2m_hw_configs[] = {
 #define M2MDEC_CLASS(NAME) \
     static const AVClass v4l2_m2m_ ## NAME ## _dec_class = { \
         .class_name = #NAME "_v4l2m2m_decoder", \
+        .item_name  = av_default_item_name, \
         .option     = options, \
         .version    = LIBAVUTIL_VERSION_INT, \
     };

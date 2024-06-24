8c5bbc3eefd2e9d494517d64ef79d6d6d8ab24fe
Scott Theisen <scott.the.elm@gmail.com>
2024-06-24 11:57:16 -0400

    libavformat/demux.c: added null checks
    originally from https://github.com/MythTV/mythtv/commit/28d2b3e87763c6102d20cfe959547b1a5f5cd609

--- libavformat/demux.c
+++ libavformat/demux.c
@@ -1303,7 +1303,8 @@ static int read_frame_internal(AVFormatContext *s, AVPacket *pkt)
                    av_ts2str(pkt->dts),
                    pkt->size, pkt->duration, pkt->flags);
 
-        if (sti->need_parsing && !sti->parser && !(s->flags & AVFMT_FLAG_NOPARSE)) {
+        if (/* MythTV null checks */ st && st->codecpar &&
+            sti->need_parsing && !sti->parser && !(s->flags & AVFMT_FLAG_NOPARSE)) {
             sti->parser = av_parser_init(st->codecpar->codec_id);
             if (!sti->parser) {
                 av_log(s, AV_LOG_VERBOSE, "parser not found for codec "

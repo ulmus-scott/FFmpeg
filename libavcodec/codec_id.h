ac4d8616d883d1e419d8dd89e40e4c8c82ef369f
Scott Theisen <scott.the.elm@gmail.com>
2024-06-24 16:21:04 -0400

    libavcodec/codec_id.h: add DSMCC_B

--- libavcodec/codec_id.h
+++ libavcodec/codec_id.h
@@ -556,6 +556,9 @@ enum AVCodecID {
     AV_CODEC_ID_MPEG2VBI,
     AV_CODEC_ID_DVB_VBI,
 
+    /* DSMCC codec */
+    AV_CODEC_ID_DSMCC_B,
+
     /* other specific kind of codecs (generally used for attachments) */
     AV_CODEC_ID_FIRST_UNKNOWN = 0x18000,           ///< A dummy ID pointing at the start of various fake codecs.
     AV_CODEC_ID_TTF = 0x18000,
